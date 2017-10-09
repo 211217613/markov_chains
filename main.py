87e3e4929d6ed274b0fe7fe828ca7ebf74c276a3
211217613 <rubenavazquez@gmail.com>
2017-10-09 08:01:21 -0700

    Build chain

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ def read_file(filename):
     with open(filename, 'r') as file:
         contents = file.read().replace('\n\n', ' ')
     return contents
+
+def build_chain(text, chain={}):
+    words = text.split(' ')
+    index = 1
+    for word in words[1::]:
+        key = words[index - 1]
+        if key in chain:
+            chain[key].append(word)
+        else:
+            chain[key] = [word]
+        index += 1
+    return chain
